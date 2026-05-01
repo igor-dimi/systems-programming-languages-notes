844c0cd1afbee17ad3dc0f2b6e2f9d398c6491a9
Igor Dimitrov <igor-dimi@protonmail.com>
2026-05-01 19:13:06 +0200

    add cmakelists, modify gitignore

---
+++ code/cpp/learning-cpp/stroustrup/ch01/ex_hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from Stroustrup ch01!\n";
+    return 0;
+}
---
+++ code/cpp/learning-cpp/stroustrup/ch01/ex_hello02.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "hey there!\n good bye!\n";
+    return 0;
+}
